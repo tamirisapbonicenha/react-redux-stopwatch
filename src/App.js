15cdf7c4276c77ea4c64e843380f1cc0d86e1f89
Tamiris Bonicenha <tamirisapbonicenha@gmail.com>
2019-07-02 18:18:26 -0300

    refactor and state in parent

--- src/App.js
+++ src/App.js
@@ -4,11 +4,41 @@ import './App.css';
 import Stopwatch from './Stopwatch';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      runningTime: 0,
+      status: false,
+    };
+    this.handleStart = this.handleStart.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleStart() {
+    this.setState(state => {
+      if (state.status) {
+        clearInterval(this.timer);
+      } else {
+        const startTime = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({ runningTime: Date.now() - startTime });
+        });
+      }
+      return { status: !state.status };
+    });
+  }
+  handleReset() {
+    clearInterval(this.timer);
+    this.setState({ runningTime: 0, status: false });
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
   render() {
+    const { status, runningTime } = this.state;
     return (
       <div className="App">
         <h1>Stopwatch</h1>
-        <Stopwatch />
+        <Stopwatch time={runningTime} status={status} start={() => this.handleStart} reset={() => this.handleReset} />
       </div>
     )
   }
--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,42 +1,13 @@
 import React, { Component } from 'react';
 
 class Stopwatch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      runningTime: 0,
-      status: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
-  handleClick() {
-    this.setState(state => {
-      if (state.status) {
-        clearInterval(this.timer);
-      } else {
-        const startTime = Date.now() - this.state.runningTime;
-        this.timer = setInterval(() => {
-          this.setState({ runningTime: Date.now() - startTime });
-        });
-      }
-      return { status: !state.status };
-    });
-  }
-  handleReset() {
-    clearInterval(this.timer);
-    this.setState({ runningTime: 0, status: false });
-  }
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
   render() {
-    const { status, runningTime } = this.state;
+    const { time, status } = this.props;
     return (
       <div>
-        <p>{runningTime}ms</p>
-        <button onClick={this.handleClick}>{status ? 'Stop' : 'Start'}</button>
-        <button onClick={this.handleReset}>Reset</button>
+        <p>{time}ms</p>
+        <button onClick={this.props.start()}>{status ? 'Stop' : 'Start'}</button>
+        <button onClick={this.props.reset()}>Reset</button>
       </div>
     );
   }
